e91b53039b86a4b6ee7659d07ece5bf17bf6bfe6
Eric Eisenhart <freiheit@gmail.com>
2017-02-05 15:32:05 -0800

    Formatting

--- discord.frog.tips.py
+++ discord.frog.tips.py
@@ -21,16 +21,17 @@ from frogsay.speech import make_frog_fresco
 # Parse the config and stick in global "config" var.
 config = configparser.ConfigParser()
 for inifile in [os.path.expanduser('~')+'/.frog.tips.ini','frog.tips.local.ini','frog.tips.ini']:
-  if os.path.isfile(inifile):
-    config.read(inifile)
-    break # First config file wins
+    if os.path.isfile(inifile):
+        config.read(inifile)
+        break # First config file wins
 MAIN = config['MAIN']
 
 def get_cache_dir(app_name='frogsay'):
     return os.path.join(click.get_app_dir(app_name), 'croak_cache')
 
 
-description = '''PLEASE READ THE FROG MANUAL AS OUR DOCUMENTATION WRITER HAS PUT HUNDREDS OF HOURS OF WORK INTO IT.'''
+description = '''PLEASE READ THE FROG MANUAL AS OUR DOCUMENTATION WRITER HAS PUT HUNDREDS OF HOURS OF WORK INTO IT.
+TO INFEST SERVER WITH FROG TIPS: https://discordapp.com/oauth2/authorize?&client_id=203243007317639168&scope=bot&permissions=3072'''
 bot = commands.Bot(command_prefix='!', description=description)
 
 @bot.event
