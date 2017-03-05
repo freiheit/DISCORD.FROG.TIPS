58cd4d5e1b2feadf2de2b05c16e083bb1bfc2833
Eric Eisenhart <freiheit@gmail.com>
2017-03-04 17:58:04 -0800

    Formatting

--- discord.frog.tips.py
+++ discord.frog.tips.py
@@ -20,11 +20,16 @@ from frogsay.speech import make_frog_fresco
 
 # Parse the config and stick in global "config" var.
 config = configparser.ConfigParser()
-for inifile in [os.path.expanduser('~')+'/.frog.tips.ini','frog.tips.local.ini','frog.tips.ini']:
+for inifile in [
+        os.path.expanduser('~') +
+        '/.frog.tips.ini',
+        'frog.tips.local.ini',
+        'frog.tips.ini']:
     if os.path.isfile(inifile):
         config.read(inifile)
-        break # First config file wins
+        break  # First config file wins
 MAIN = config['MAIN']
+
 
 def get_cache_dir(app_name='frogsay'):
     return os.path.join(click.get_app_dir(app_name), 'croak_cache')
@@ -32,7 +37,10 @@ def get_cache_dir(app_name='frogsay'):
 
 description = '''PLEASE READ THE FROG MANUAL AS OUR DOCUMENTATION WRITER HAS PUT HUNDREDS OF HOURS OF WORK INTO IT.
 TO INFEST SERVER WITH FROG TIPS: https://discordapp.com/oauth2/authorize?&client_id=203243007317639168&scope=bot&permissions=3072'''
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description=description)
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or('!'),
+    description=description)
+
 
 @bot.event
 @asyncio.coroutine
@@ -42,6 +50,7 @@ def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.group(pass_context=True)
 @asyncio.coroutine
 def tip(ctx):
@@ -50,6 +59,7 @@ def tip(ctx):
     with open_client(cache_dir=get_cache_dir()) as client:
         tip = client.frog_tip()
     yield from bot.say(tip)
+
 
 @bot.group(pass_context=True)
 @asyncio.coroutine
@@ -71,6 +81,7 @@ def fresco(ctx):
 # @bot.group(pass_context=True)
 # @asyncio.coroutine
 # def help(ctx):
-#    yield from bot.say('FROG.TIPS BOT ONLY KNOWS HOW TO !TIP AND HOW TO !FRESCO')
+# yield from bot.say('FROG.TIPS BOT ONLY KNOWS HOW TO !TIP AND HOW TO
+# !FRESCO')
 
 bot.run(MAIN.get('login_token'))
